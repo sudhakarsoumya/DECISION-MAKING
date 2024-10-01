a95fb62ce304a25ba5fdab1ed81be9fb752d3953
sudhakarsoumya <22hr1a0519@mtieat.org>
2024-10-01 21:42:10 +0530

    Q8.py

---
+++ Q8.py
@@ -0,0 +1,34 @@
+...
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z respectively. One of the 3 labs has been allocated for ACE training. Out of the available labs, find the lab which has minimal seating capacity.
+Input format:
+Input consists of 3 integers and a string
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the lab which is allocated for ACE training
+Output format:
+Print the minimal seating capacity lab amongst the available labs.
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+L3
+Sample Output:
+L1
+...
+
+L1 = int(input())
+L2 = int(input())
+L3 = int(input())
+allocated_lab = input() 
+capacities = {
+    "L1": L1,
+    "L2": L2,
+    "L3": L3
+}
+
+if allocated_lab in capacities:
+    del capacities[allocated_lab]
+min_lab = min(capacities, key=capacities.get)
+print(min_lab)
